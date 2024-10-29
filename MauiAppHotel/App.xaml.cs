82a99f145e0879d36845fcd329f7b22f66dd966c
Wilker <wilkerbranquinho@gmail.com>
2024-10-29 19:22:52 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿
+namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState) //sobrescrevendo o método
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;  //largura de janela
+            window.Height = 600; //altura de janela
+            return window;
+        }
+    }
+}
